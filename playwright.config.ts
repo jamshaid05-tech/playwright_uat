14a7ef3c34635ea1ca819e622c6606a5be8bcafc
jamshaid05-tech <jbhati7860@gmail.com>
2026-05-14 23:06:57 +0500

    wip:added forgotpassword, login, register testcases

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -78,6 +78,7 @@ export default defineConfig({
     {
       name: 'Microsoft Edge',
       use: { ...devices['Desktop Edge'], channel: 'msedge' },
+      dependencies: ['setup'],
     },
     // {
     //   name: 'Google Chrome',
