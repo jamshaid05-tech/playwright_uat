b1ec8da118de5277b86ea65d3e3fe36ebab812b0
jamshaid05-tech <jbhati7860@gmail.com>
2026-06-26 23:35:05 +0500

    ci: pass EMAIL and PASSWORD secrets to workflow

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-dotenv.config();
+dotenv.config({ override: false });
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
